80c13101667c6361eb2f86ad00f44503bb1856c5
nareshbhusal <devnbhusal@gmail.com>
2022-05-08 14:09:42 +0530

    Replace resize-observer to update beacon position with autoUpdate method from floating-ui

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -2,7 +2,6 @@ import htmlStringToElement from "./htmlStringToElement";
 import mergeObjects from "./mergeObjects";
 import styleObjectToString from "./styleObjectToString";
 import getStepUID from "./getStepUID";
-import onElementResize from "./onElementResize";
 import getElementPosition from "./getElementPosition";
 import doesStringMatchRegex from "./doesStringMatchRegex";
 import changeAsyncStepStatus from "./changeAsyncStepStatus";
@@ -20,7 +19,6 @@ export {
     mergeObjects,
     styleObjectToString,
     getStepUID,
-    onElementResize,
     getElementPosition,
     doesStringMatchRegex,
     changeAsyncStepStatus,
--- src/hotspot/Hotspot.ts
+++ src/hotspot/Hotspot.ts
@@ -1,24 +1,26 @@
 import { document, window } from "global";
 import createHotspotTooltip from "./createHotspotTooltip";
 import { createBeaconElement, positionBeacon } from "./beacon-element";
+import { autoUpdate } from '@floating-ui/dom';
 import {
     getElementPosition,
     getStepUID,
     changeAsyncStepStatus,
-    onElementResize,
+    debounce
 } from "../common/utils";
 import { Hotspot as HotspotData } from "../common/types";
 import { log, warn, error } from "../common/logger";
 
 // TODO_: In case of customizing hotspot's beacon, we can just have a beaconElement property
 // TODO: Should the beacon animation stop when tip is open?
+
 class Hotspot {
     private tipID: string;
     private fuitInstance: any;
     private targetElement: HTMLElement;
     readonly data: HotspotData;
     private beaconID: string;
-    private resizeObservers: any[] = [];
+    private beaconAutoUpdateCleanup!: Function;
 
     constructor({ data, guideID }) {
         log(data);
@@ -32,19 +34,7 @@ class Hotspot {
         });
         this.targetElement = document.querySelector(target.elementSelector);
         this.addBeacon();
-
-        // reposition beacon on body and targetElement resize
-        /* this.resizeObservers.push(onElementResize(document.body, this.repositionBeacon.bind(this)));
-        this.resizeObservers.push(
-            onElementResize(this.targetElement, this.repositionBeacon.bind(this)),
-        ); */
-    }
-
-    private repositionBeacon(): void {
-        this.remove();
-        this.fuitInstance = null;
-        this.addBeacon();
-        log("reset beacon position");
+        this.createTip();
     }
 
     private updateBeaconPosition() : void {
@@ -64,7 +54,6 @@ class Hotspot {
 
         const beaconElement = document.getElementById(this.beaconID);
         positionBeacon(beaconElement, targetPosition, beaconData.placement);
-        log('updated beacon position');
     }
 
     private addBeacon(): void {
@@ -78,44 +67,63 @@ class Hotspot {
             toggleTooltip: this.toggleTooltip.bind(this),
         });
         this.updateBeaconPosition();
-        window.ub = this.updateBeaconPosition.bind(this);
+    }
+
+    private async createTip() {
+        const Lusift = window["Lusift"];
+        const target = document.getElementById(`${this.beaconID}`);
+        const { data, styleProps } = this.data.tip;
+        const isDevMode = !Boolean(Lusift.activeGuide);
+
+        // do not allow async step to close in dev mode
+        const removeMethod = isDevMode ? Lusift.close : this.removeAndCloseAsync.bind(this);
+
+        this.fuitInstance = await createHotspotTooltip({
+            remove: removeMethod,
+            uid: this.tipID,
+            index: this.data.index,
+            target: target,
+            styleProps,
+            showOnCreate: false,
+            data,
+            onClickOutside: (instance, event) => {
+                removeMethod();
+            }
+        });
+
+        // reposition beacon on body and targetElement resize
+
+        const updateBeaconPosition = this.updateBeaconPosition.bind(this);
+
+        const debouncedUpdateBeaconPosition = debounce((_x?) => {
+            updateBeaconPosition();
+        }, 100);
+
+        this.beaconAutoUpdateCleanup = autoUpdate(
+            this.targetElement,
+            this.fuitInstance.tooltipElement,
+            () => {
+                debouncedUpdateBeaconPosition(undefined);
+            },
+            {
+                ancestorScroll: false,
+                ancestorResize: true,
+                elementResize: true
+            });
     }
 
     private async toggleTooltip() {
         // log('toggle tooltip');
 
-        const target = document.getElementById(this.beaconID);
-        const { data, styleProps } = this.data.tip;
         const activeHotspot = window.Lusift.activeHotspot;
         const Lusift = window["Lusift"];
 
         if (activeHotspot && this.data.index !== activeHotspot.data.index) {
+            // hide any other potentially active hotspot tip
             Lusift.activeHotspot.hideTooltip();
         }
 
-        // do not allow async step to close in dev mode
-        const isDevMode = !Boolean(Lusift.activeGuide);
-        let removeMethod = isDevMode ? Lusift.close : this.removeAndCloseAsync.bind(this);
-
-        if (!this.fuitInstance) {
-            // if it was never initiated
-            this.fuitInstance = await createHotspotTooltip({
-                remove: removeMethod,
-                uid: this.tipID,
-                index: this.data.index,
-                target: target,
-                styleProps,
-                showOnCreate: false,
-                data,
-                onClickOutside: (instance, event) => {
-                    log('clicked outside');
-                    // TODO: only when it's not hidden
-                    console.log(event);
-                    // removeMethod();
-                }
-            });
-            Lusift.activeHotspot = this;
-        } else if (this.fuitInstance.getState().isRemoved) {
+        if (this.fuitInstance.getState().isRemoved) {
             error("Uh... but it doesn't exist. unexpected");
             // if it's removed
         } else if (this.fuitInstance.getState().isShown) {
@@ -140,23 +148,25 @@ class Hotspot {
 
     private remove(): void {
         log(`Removing id: ${this.data.index} hotspot`);
+
         if (this.fuitInstance) {
+            // TODO: Ensure tooltip was hidden with this.hideTooltip() before removing
             if (this.fuitInstance.getState().isRemoved) {
                 log("Hotspot's tooltip is already destroyed");
+            } else {
+                this.fuitInstance.remove();
             }
-            this.fuitInstance.remove();
         } else {
             log("Hotspot closed without ever opening");
         }
         const beaconElement = document.getElementById(this.beaconID);
         if (beaconElement) {
             beaconElement.parentElement.remove();
-            window.Lusift.activeHotspot = null;
         }
     }
 
     private removeResizeObservers(): void {
-        this.resizeObservers.forEach(ro => ro.disconnect());
+        this.beaconAutoUpdateCleanup();
     }
 
     private removeAndCloseAsync(): void {
